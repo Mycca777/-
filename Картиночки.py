f1a394647c0a07758648361d8af13b0efcc3daee
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2020-01-23 22:28:21 +0300

    Update Картиночки.py

--- Картиночки.py
+++ Картиночки.py
@@ -14,6 +14,8 @@ for i in range(10):
     y += 40
 a[0].save('moving_ball.gif', format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
 # 4 задание ---------------------------------------------------------------------------------------------
+from PIL import Image, ImageDraw
+
 myimage = Image.open("Elbrus.jpg")
 myimage.resize((1,1))
 draw = ImageDraw.Draw(myimage)
@@ -24,5 +26,21 @@ for i in range(width):
         a = pix[i, j][0]
         b = pix[i, j][1]
         c = pix[i, j][2]
-        draw.point((i, j), (255 - a, 255 - b, 255 - c))
+        draw.point((j, i), (255 - c, 255 - a, 255 - b))
 myimage
+# 9 задание----------------------------------------------------------------------------------------------
+from PIL import Image, ImageDraw, ImageFont
+def zadanie9(a):
+    mode = 'RGB'
+    size = 400,400
+    img0 = Image.new(mode, size, color = 0).convert('RGBA')
+    myname = Image.new('RGBA', img0.size, (255,255,255,0))
+    
+    font = ImageFont.load_default()
+    draw = ImageDraw.Draw(myname)
+    draw.text((100,100), "Mycca", font=font, fill=(255,255,255,255))
+    
+    result = Image.alpha_composite(img0, myname)
+    result = result.transpose(Image.FLIP_LEFT_RIGHT)
+    result.save(a)
+zadanie9("file1.png")
